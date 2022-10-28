5ba4424417d91616e0743c2704bc147673cc93b1
jokay03J-V2 <jmagro@edenschool.fr>
2022-10-28 14:48:28 +0200

    bonus 1

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 import "../css/style.css";
 
 const divs = document.getElementsByTagName("div");
+const result = document.getElementById("result");
+let prevColor = null;
 
 for (let index = 0; index < divs.length; index++) {
     const element = divs[index];
     element.addEventListener("click", (ev) => {
-        document.getElementById("result").textContent = `vous avez cliquer sur ${ev.target.id}`
+        if (prevColor === ev.target.id) { 
+            result.textContent = "Vous ne pouvez pas cliquer sur le même carré Veuillez cliquer sur un carré"
+        console.log(prevColor === ev.target.id);
+        }else {
+        result.textContent = `vous avez cliquer sur ${ev.target.id}`
+        prevColor = ev.target.id
+        }
     })
 }
